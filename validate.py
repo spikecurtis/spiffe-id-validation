5e4d8de2ae175f5eaea6390c17ed50d6ec710a09
Spike Curtis <spike@lodestar.ai>
2022-03-03 14:04:49 -0800

    Remove unhittable, add more test cases, clarify characters vs bytes
    Signed-off-by: Spike Curtis <spike@lodestar.ai>

--- validate.py
+++ validate.py
@@ -1,6 +1,13 @@
 # This file contains an example algorithm for validating SPIFFE-IDs using only basic string-manipulation
 # routines, without any dependencies on URL parsing libraries, regular expressions, etc.  It is written
 # in Python, but designed to be easily translated into other languages.
+#
+# IMPORTANT NOTE: this algorithm and the subroutines `substring`, `character_at` and `len` operate on
+# sequences of _characters_, which is not necessarily equivalent to operating on _bytes_, depending on the
+# encoding.  (For example, if the string is encoded in UTF-8, the 5th character might not correspond to the 5th
+# byte.)  Any sequence of _bytes_ purporting to be a SPIFFE-ID needs to first be decoded into a sequence
+# of characters before it can be validated by this method, which means the validator must know the character
+# encoding to expect. 
 
 def substring(target, start, end):
     return target[start:end]
@@ -8,8 +15,8 @@ def substring(target, start, end):
 def character_at(target, num):
     return target[num]
 
-AUTHORITY_LETTERS = "abcdefghijklmnopqrstuvwxyz0123456789.-_"
-PATH_SEGMENT_LETTERS = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789.-_"
+AUTHORITY_CHARS = "abcdefghijklmnopqrstuvwxyz0123456789.-_"
+PATH_SEGMENT_CHARS = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz0123456789.-_"
 
 
 # Returns  valid (bool), authority (string), path (string).  If valid is False, authority and path are set to None.
@@ -32,7 +39,7 @@ def validate(spiffe_id):
             break
         # Note here that since valid letters does not include @ or :,
         # this ensures that userinfo and port are not set in the authority 
-        if c not in AUTHORITY_LETTERS:
+        if c not in AUTHORITY_CHARS:
             return False, None, None
         cursor += 1
     authority = substring(spiffe_id, 9, cursor)
@@ -43,14 +50,14 @@ def validate(spiffe_id):
         return False, None, None
 
     # A SPIFFE-ID that includes only the authority (empty-path) is valid.
-    if cursor == len(spiffe_id):
+    if len(path) == 0:
         return True, authority, path
 
     # per RFC3986, since the authority is not empty, the path must be empty or 
-    # begin with a slash.  We know from above that is it not empty, so it must
-    # begin with a slash.
-    if character_at(spiffe_id, cursor) != '/':
-        return False, None, None
+    # begin with a slash.  In order to exit the while loop above, we must either
+    # encounter a slash, or the end of the string (and thus the path is empty).
+    # Since we check above for an empty path, we know the path begins with a slash
+    # as required.
 
     # We now check the path, character by character, and segment by segment.
     cursor += 1
@@ -65,7 +72,7 @@ def validate(spiffe_id):
             cursor += 1
             segment_start = cursor
             continue
-        if c not in PATH_SEGMENT_LETTERS:
+        if c not in PATH_SEGMENT_CHARS:
             return False, None, None
         cursor += 1
     
@@ -96,8 +103,13 @@ if __name__ == "__main__":
         ("spiffe://foo.bar/Baz/buZ/", (False, None, None)), 
         ("spiffe://foo.bar//buZ/", (False, None, None)), 
         ("spiffe://foo.bar/../buZ/", (False, None, None)), 
+        ("spiffe://foo.bar/..Baz/.buZ", (True, "foo.bar", "/..Baz/.buZ")), 
         ("spiffe://foo.bar/buZ/%2d", (False, None, None)), 
         ("Spiffe://foo.bar/Baz/buZ", (False, None, None)), 
+        ("spiffe://domain.test/path/validate?query=1", (False, None, None)),
+        ("spiffe://domain.test/path/validate?#fragment-1", (False, None, None)),
+        ("spiffe://domain.test:8080/path/validate", (False, None, None)),
+        ("spiffe://user:password@test.org/path/validate", (False, None, None)),
     ]
     for e, r in examples:
         v = validate(e)
